cc0a9a46532bb7f31ea7b12cc26a8a39203fba75
dlof-lib <javadlof8@gmail.com>
2026-08-07 21:17:48 +0300

    Fix: add androidx.compose.runtime.getValue import for State delegate in SkeletonLoader.kt

--- app/src/main/java/com/yeex/dlof/ui/components/SkeletonLoader.kt
+++ app/src/main/java/com/yeex/dlof/ui/components/SkeletonLoader.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
