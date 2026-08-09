e98a38c6ecfe8bceb22b4db20706b0e9ad915033
dlof-lib <javadlof8@gmail.com>
2026-08-09 20:04:46 +0300

    Initial commit from ZIP upload

--- app/src/main/java/com/yeex/dlof/ui/search/SearchScreen.kt
+++ app/src/main/java/com/yeex/dlof/ui/search/SearchScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
