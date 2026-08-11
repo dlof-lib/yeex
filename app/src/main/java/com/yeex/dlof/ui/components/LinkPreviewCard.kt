8bae07ecf0ed9de216a90b0ab1fe16149c9932d4
dlof-lib <javadlof8@gmail.com>
2026-08-11 15:26:30 +0300

    Update LinkPreviewCard.kt

--- app/src/main/java/com/yeex/dlof/ui/components/LinkPreviewCard.kt
+++ app/src/main/java/com/yeex/dlof/ui/components/LinkPreviewCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.Image
