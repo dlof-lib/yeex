046eb444eb4403bbf050200a7f33d78d800effc2
dlof-lib <javadlof8@gmail.com>
2026-08-12 01:51:06 +0300

    Initial commit from ZIP upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
